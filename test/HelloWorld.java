019b4688d60a38dbabe9dd36d100003345a76662
renxuelei <xuelei77@hotmail.com>
2017-10-11 15:55:03 +0800

    test: import hello_g4_jar.

--- test/HelloWorld.java
+++ test/HelloWorld.java
@@ -1,5 +1,10 @@
+import java.io.*;
+import org.antlr.runtime.*;
+import com.rxl.study.*;
+
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello, world!");
+        HelloLexer lex = new HelloLexer(null);
     }
 }
